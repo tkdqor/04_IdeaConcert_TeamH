54d7e3605537345c950a646b047ce225f4db89c1
Github <weeds1590@gmail.com>
2022-07-18 11:01:16 +0900

    Feat : HeeSeok hello를 리턴하는 api 추가

---
+++ hello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from hello.views import GetHello, RedirectHello
+
+app_name = "hello"
+
+urlpatterns = [
+    path("", RedirectHello.as_view(), name="redirect_hello"),
+    path("IP/api/hello", GetHello.as_view(), name="hello"),
+]
--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,15 @@
-# from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from rest_framework import status
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class GetHello(APIView):
+    def get(self, request):
+        return HttpResponse("hello", status=status.HTTP_200_OK)
+
+
+class RedirectHello(APIView):
+    def get(self, request):
+        return redirect("hello:hello", status=status.HTTP_200_OK)
