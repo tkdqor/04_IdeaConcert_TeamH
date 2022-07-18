d87eaf921c6d0fdec1d905d5bdd21afcf63923de
minji <my970524@naver.com>
2022-07-18 15:23:42 +0900

    Docs : config/settings/prod.py 수정
    - static 파일 경로 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 STATIC_URL = "/static/"
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    BASE_DIR / "static",
 ]
