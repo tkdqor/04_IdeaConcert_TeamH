b36f02b1369c3196e9026af8b75e8543b31e1411
minji <my970524@naver.com>
2022-07-18 19:48:19 +0900

    Docs : settings 수정
    - prod.py 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,10 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["13.209.234.237", "django"]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
