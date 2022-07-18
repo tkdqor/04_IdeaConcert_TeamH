89a009cfb7e1552767d2e7e35ddc27391a866150
김훈희 <nmdkims@gmail.com>
2022-07-18 11:59:03 +0900

    Feat : hello api 추가
    - IP/api/hello/ 경로에서 hello가 response 되는 api 구성

---
+++ hello/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import HelloAPIView
+
+app_name = "hello"
+
+urlpatterns = [
+    path("hello", HelloAPIView.as_view()),
+]
--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+# url : GET api/v1/bossRaid
+class HelloAPIView(APIView):
+    """
+    Assignee : 훈희
+    Hello message를 내보내는 api view 입니다.
+    """
+
+    def get(self, request):
+
+        return Response({"Hello"}, status=status.HTTP_200_OK)
