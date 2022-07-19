e0d66bc5eb40d539bab74445e3744c7816a25271
sangbaek <aumsbk@naver.com>
2022-07-19 17:33:19 +0900

    Rename : url 주소 변경 - urls.py의 url 주소 변경

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('IP/api/hello', include("idea.urls")),
+    path('api/hello', include("idea.urls")),
 ]
